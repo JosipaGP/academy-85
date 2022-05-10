76ca1f641c603b5e9af09808f16857ca94acfd28
JosipaGP <josipa.gregurovic.com>
2022-05-10 03:45:44 +0200

    updated

---
+++ e1.dart
@@ -0,0 +1,12 @@
+
+void main(){
+var firstName = "Josipa";
+var lastName = "Gregurovic Podgajski";
+var age = 35;
+var height = 1.55;
+
+print (firstName);
+print (lastName);
+print (age);
+print (height);
+}
---
+++ e4.dart
@@ -0,0 +1,18 @@
+int netSalary = 3000; 
+int expenses = 2000;
+
+
+void main(){  
+  if (netSalary > expenses){
+       print ("You have saved ${netSalary-expenses} this month");
+ }
+  else if
+    (expenses>netSalary){
+     print ("You have lost ${expenses-netSalary} this month");
+  }
+  else{
+    print
+      ("Your balance hasn't changed");
+  }
+    
+  }
